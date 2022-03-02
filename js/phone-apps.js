cb5a4ce9a0f8e9d31fbf641d3009593033a687ba
ronymia <official.mdronymia@gmail.com>
2022-03-02 15:59:31 +0600

    comment

--- js/phone-apps.js
+++ js/phone-apps.js
@@ -3,7 +3,7 @@ const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(res => res.json())
         .then(phone => displaySearchResult(phone.data));
@@ -18,13 +18,13 @@ const displaySearchResult = data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-80 shadow p-3 mb-5 bg-body rounded">
+        <div  class="card h-80 shadow p-3 mb-5 bg-body rounded">
                 <img src="${data.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title ">${data.brand}</h5>
                     <div class ="d-flex justify-content-between">
                     <span class="card-text">${data.phone_name}</span>
-                    <button type="button" class="btn btn-light">see details</button>
+                    <button onclick="loadPhoneDetails(${data.slug})" type="button" class="btn btn-light">see details</button>
                     </div>
                 </div>
             </div>
@@ -35,3 +35,6 @@ const displaySearchResult = data => {
 };
 
 // details data
+const loadPhoneDetails = phoneId => {
+    console.log(phoneId);
+}

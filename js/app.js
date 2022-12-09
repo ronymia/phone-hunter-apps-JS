e171eb9bc7ad70e4abe5286e694d326864abe766
ronymia <mdronymia.javascript.com>
2022-12-09 11:00:41 +0600

    load phones

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 
 const searchPhone = () => {
-    const searchFeild = document.getElementById("search-field").value;
-    console.log(searchFeild);
+    const searchText = document.getElementById("search-field").value;
+    // console.log(searchFeild);
+
+    fetch(` https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then(res => res.json())
+        .then(data => loadPhones(data.data))
+}
+
+
+const loadPhones = (phones) => {
+    // console.log(phones);
+
 }
